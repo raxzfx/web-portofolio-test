a904daaf06d6568d7608f500559573c7655e39cc
rasya nazra <rasyanazraditya@gmail.com>
2024-09-02 16:35:38 +0700

    menambahkan file

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+//hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navmenu = document.querySelector('#nav-menu')
+
+hamburger.addEventListener('click',function(){
+  hamburger.classList.toggle('hamburger-active'); 
+  navmenu.classList.toggle('hidden'); 
+});
+
+//fix nav
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixnav = header.offsetTop;
+
+    if(window.pageYOffset > fixnav){
+        header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+}
